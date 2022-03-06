181a454e8dd81497de8d0f93c7b43064f7314ea0
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2022-03-06 11:40:27 +0100

    Add realtime mode

--- MainForm.cs
+++ MainForm.cs
@@ -8,7 +8,7 @@ namespace ZumaBot2 {
         private Point windowLocation;
         private bool foundGameWindow = false;
 
-        private Bitmap gameWindow = new Bitmap(640, 480, PixelFormat.Format32bppArgb);
+        private Bitmap gameWindow = new Bitmap(640, 480, PixelFormat.Format32bppRgb);
         private Thread captureThread;
 
         private const int defaultFrameDelay = 16;
@@ -18,6 +18,17 @@ namespace ZumaBot2 {
         }
 
         private void MainForm_Load(object sender, EventArgs e) {
+            FindGameWindow();
+
+            captureThread = new Thread(() => CaptureThread());
+            captureThread.Start();
+        }
+
+        private string GetAssetPath(string filename) {
+            return Path.Combine(Directory.GetCurrentDirectory(), "images", filename);
+        }
+
+        private void FindGameWindow() {
             Bitmap screen = new Bitmap(1920, 1080, PixelFormat.Format32bppRgb);
 
             using var mTemplate = Cv2.ImRead(GetAssetPath("game_window_target_mt.png"));
@@ -67,13 +78,6 @@ namespace ZumaBot2 {
                     textDotsIndex = 0;
                 }
             }
-
-            //captureThread = new Thread(() => CaptureThread());
-            //captureThread.Start();
-        }
-
-        private string GetAssetPath(string filename) {
-            return Path.Combine(Directory.GetCurrentDirectory(), "images", filename);
         }
 
         private void Capture() {
@@ -141,11 +145,117 @@ namespace ZumaBot2 {
         }
 
         private void CaptureThread() {
-            /*while (true) {
-                using (Graphics g = Graphics.FromImage(gameWindow)) {
-                    g.CopyFromScreen(windowLocation.Value.X, windowLocation.Value.Y, 0, 0, new System.Drawing.Size(640, 480));
+            using Graphics g = Graphics.FromImage(gameWindow);
+            Color[] colorMatchers = new Color[] {
+                // Blues
+                ColorTranslator.FromHtml("#40f9ff"),
+                ColorTranslator.FromHtml("#3d3dbe"),
+
+                // Greens
+                ColorTranslator.FromHtml("#48fb67"),
+                ColorTranslator.FromHtml("#388047"),
+
+                // Yellows
+                ColorTranslator.FromHtml("#fffe2d"),
+                ColorTranslator.FromHtml("#a77c17"),
+
+                // Violets
+                ColorTranslator.FromHtml("#ff9dff"),
+                ColorTranslator.FromHtml("#a613b2"),
+
+                // Reds
+                ColorTranslator.FromHtml("#dc7e6e"),
+                ColorTranslator.FromHtml("#ae0812")
+            };
+
+            Color[] fixedColors = new Color[] {
+                Color.Blue,
+                Color.Blue,
+                Color.Green,
+                Color.Green,
+                Color.Yellow,
+                Color.Yellow,
+                Color.Violet,
+                Color.Violet,
+                Color.Red,
+                Color.Red
+            };
+
+            while (true) {
+                g.CopyFromScreen(windowLocation.X, windowLocation.Y, 0, 0, gameWindow.Size);
+
+                // Convert the game capture, but only pay attention to the saturation channel (balls are more saturated than the backgrounds)
+                using var mGameOriginal = gameWindow.ToMat();
+                using var mGameColor = gameWindow.ToMat();
+                using var mGame = gameWindow
+                    .ToMat()
+                    .CvtColor(ColorConversionCodes.RGB2HSV)
+                    .ExtractChannel(1)
+                    .Threshold(100, 255, ThresholdTypes.Binary);
+
+                // Get rid of small noise speckles
+                Cv2.FilterSpeckles(mGame, 0, 16, 8);
+                
+                // Fill holes
+                using Mat kernel = Cv2.GetStructuringElement(MorphShapes.Ellipse, new OpenCvSharp.Size(8, 8));
+                Cv2.MorphologyEx(mGame, mGame, MorphTypes.Close, kernel);
+
+                // Get largest connected component (the ball chain)
+                ConnectedComponents cc = Cv2.ConnectedComponentsEx(mGame, PixelConnectivity.Connectivity4);
+                using Mat mGameLargestBlob = new Mat();
+                cc.FilterByBlob(mGame, mGameLargestBlob, cc.GetLargestBlob());
+
+                // Create a composite of the previous stap (binary largest blob image + original color)
+                Cv2.BitwiseAnd(mGameColor, mGameLargestBlob.CvtColor(ColorConversionCodes.GRAY2BGR), mGameColor);
+
+                // Color them ballz
+                CircleSegment[] circles = Cv2.HoughCircles(mGameLargestBlob, HoughModes.Gradient, 5, 20, 30, 35, 0, 20);
+                foreach (CircleSegment segment in circles) {
+                    int col = mGameLargestBlob.At<int>((int)segment.Center.Y, (int)segment.Center.X);
+
+                    if (col < 0) {
+                        Vec3b ccol = mGameOriginal.At<Vec3b>((int)segment.Center.Y, (int)segment.Center.X);
+                        Color ccolAsColor = Color.FromArgb(ccol[2], ccol[1], ccol[0]);
+                        Color usedColor = Color.Orange;
+                        int usedColorIndex = -1;
+                        float lowestDistance = float.MaxValue;
+                        for (int k = 0; k < colorMatchers.Length; k++) {
+                            Color cs = colorMatchers[k];
+
+                            float colorDist = MathF.Sqrt(
+                                MathF.Pow(ccolAsColor.R - cs.R, 2) +
+                                MathF.Pow(ccolAsColor.G - cs.G, 2) +
+                                MathF.Pow(ccolAsColor.B - cs.B, 2)
+                            );
+
+                            if (colorDist < lowestDistance) {
+                                usedColor = cs;
+                                usedColorIndex = k;
+                                lowestDistance = colorDist;
+                            }
+                        }
+
+                        if (usedColorIndex >= 0) {
+                            Color fixedColor = fixedColors[usedColorIndex];
+
+                            Cv2.Circle(
+                                mGameColor,
+                                (int)segment.Center.X,
+                                (int)segment.Center.Y,
+                                (int)segment.Radius,
+                                new Scalar(fixedColor.B, fixedColor.G, fixedColor.R),
+                                2
+                            );
+                        }
+                    }
                 }
-            }*/
+
+
+                this.Invoke(() => {
+                    Cv2.ImShow("out", mGameColor);
+                    Cv2.WaitKey(defaultFrameDelay);
+                });
+            }
         }
     }
 }
